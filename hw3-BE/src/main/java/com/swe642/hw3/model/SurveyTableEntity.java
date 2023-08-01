e91addd069876b2ec3188d028cb1dba98740320c
Lord Mendoza <lord.d.mendoza@gmail.com>
2023-07-31 20:30:52 -0400

    Revised entity to add new columns

--- hw3-BE/src/main/java/com/swe642/hw3/model/SurveyTableEntity.java
+++ hw3-BE/src/main/java/com/swe642/hw3/model/SurveyTableEntity.java
@@ -1,6 +1,7 @@
 package com.swe642.hw3.model;
 
 import java.sql.Date;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,129 +12,160 @@ import jakarta.persistence.Table;
 
 
 @Entity
-@Table(name="survey_table")
+@Table(name = "survey_table")
 public class SurveyTableEntity {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(name="first_name", nullable=false)
-	private String firstName;
-	
-	@Column(name="last_name", nullable=false)
-	private String lastName;
-	
-	@Column(name="street_address", nullable=false)
-	private String streetAddress;
-	
-	@Column(name="city", nullable=false)
-	private String city;
-	
-	@Column(name="zip", nullable=false)
-	private int zip;
-	
-	@Column(name="state", nullable=false)
-	private String state;
-	
-	@Column(name="phone", nullable=false)
-	private String telephoneNo;
-	
-	@Column(name="email", nullable=false)
-	private String email;
-	
-	@Column(name="date", nullable=false)
-	private Date dateOfSurvey;
 
-	public Long getId() {
-		return id;
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  @Column(name = "first_name", nullable = false)
+  private String firstName;
 
-	public String getFirstName() {
-		return firstName;
-	}
+  @Column(name = "last_name", nullable = false)
+  private String lastName;
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+  @Column(name = "street_address", nullable = false)
+  private String streetAddress;
 
-	public String getLastName() {
-		return lastName;
-	}
+  @Column(name = "city", nullable = false)
+  private String city;
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+  @Column(name = "zip", nullable = false)
+  private int zip;
 
-	public String getStreetAddress() {
-		return streetAddress;
-	}
+  @Column(name = "state", nullable = false)
+  private String state;
 
-	public void setStreetAddress(String streetAddress) {
-		this.streetAddress = streetAddress;
-	}
+  @Column(name = "phone", nullable = false)
+  private String telephoneNo;
 
-	public String getCity() {
-		return city;
-	}
+  @Column(name = "email", nullable = false)
+  private String email;
 
-	public void setCity(String city) {
-		this.city = city;
-	}
+  @Column(name = "date", nullable = false)
+  private Date dateOfSurvey;
+  @Column(name = "likedMostAboutUniversity")
+  private List<String> likedMostAboutUniversity;
+  @Column(name = "recommendLikelihood")
+  private String recommendLikelihood;
+  @Column(name = "moreFeedback")
+  private String moreFeedback;
 
-	public int getZip() {
-		return zip;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public void setZip(int zip) {
-		this.zip = zip;
-	}
-	
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public String getState() {
-		return state;
-	}
+  public String getFirstName() {
+    return firstName;
+  }
 
-	public void setState(String state) {
-		this.state = state;
-	}
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-	public String getTelephoneNo() {
-		return telephoneNo;
-	}
+  public String getLastName() {
+    return lastName;
+  }
 
-	public void setTelephoneNo(String telephoneNo) {
-		this.telephoneNo = telephoneNo;
-	}
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-	public Date getDateOfSurvey() {
-		return dateOfSurvey;
-	}
+  public String getStreetAddress() {
+    return streetAddress;
+  }
 
-	public void setDateOfSurvey(Date dateOfSurvey) {
-		this.dateOfSurvey = dateOfSurvey;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
+  public void setStreetAddress(String streetAddress) {
+    this.streetAddress = streetAddress;
+  }
 
-	public void setEmail(String email) {
-		this.email = email;
+  public String getCity() {
+    return city;
+  }
 
-	}
+  public void setCity(String city) {
+    this.city = city;
+  }
 
-	@Override
-	public String toString() {
-		return "SurveyTableEntity [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName
-				+ ", streetAddress=" + streetAddress + ", city=" + city + ", zip=" + zip + ", state=" + state
-				+ ", telephoneNo=" + telephoneNo + ", email=" + email + ", dateOfSurvey=" + dateOfSurvey + "]";
-	}
-	
-	
+  public int getZip() {
+    return zip;
+  }
+
+  public void setZip(int zip) {
+    this.zip = zip;
+  }
+
+
+  public String getState() {
+    return state;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public String getTelephoneNo() {
+    return telephoneNo;
+  }
+
+  public void setTelephoneNo(String telephoneNo) {
+    this.telephoneNo = telephoneNo;
+  }
+
+  public Date getDateOfSurvey() {
+    return dateOfSurvey;
+  }
+
+  public void setDateOfSurvey(Date dateOfSurvey) {
+    this.dateOfSurvey = dateOfSurvey;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public List<String> getLikedMostAboutUniversity() {
+    return likedMostAboutUniversity;
+  }
+
+  public void setLikedMostAboutUniversity(List<String> likedMostAboutUniversity) {
+    this.likedMostAboutUniversity = likedMostAboutUniversity;
+  }
+
+  public String getRecommendLikelihood() {
+    return recommendLikelihood;
+  }
+
+  public void setRecommendLikelihood(String recommendLikelihood) {
+    this.recommendLikelihood = recommendLikelihood;
+  }
+
+  public String getMoreFeedback() {
+    return moreFeedback;
+  }
+
+  public void setMoreFeedback(String moreFeedback) {
+    this.moreFeedback = moreFeedback;
+  }
+
+  @Override
+  public String toString() {
+    return "SurveyTableEntity [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName
+      + ", streetAddress=" + streetAddress + ", city=" + city + ", zip=" + zip + ", state=" + state
+      + ", telephoneNo=" + telephoneNo + ", email=" + email + ", dateOfSurvey=" + dateOfSurvey
+      + ", likedMostAboutUniversity=" + likedMostAboutUniversity.toString() + ", recommendLikelihood="
+      + recommendLikelihood + ", moreFeedback=" + moreFeedback
+      + "]";
+  }
+
 
 }
